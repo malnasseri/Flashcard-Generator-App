e88ee12cbb54e6e40e45631706bdbe44be003a14
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2017-06-21 18:51:51 -0400

    added some comments

--- CLI.js
+++ CLI.js
@@ -1,6 +1,6 @@
 // Require the 'inquirer' npm
 var inquirer = require("inquirer");
-
+ 
 
 //importing our instructors
 var flashCards = require("./flashCards.js");
@@ -26,89 +26,90 @@ for (var i = 0; i < questions.length; i++) {
 
 	//creating a function that's going to ask a question
  function askQuestion() {
- 	//using inquirer to display the question and at the same time take the answer as input
-	inquirer.prompt([
-		{
-			type: 'input',
-			message: clozeQuestions[currentQuestion].partial + '\nAnswer: ',
-			name: 'userAnswer'
-		}
-	]).then(function (answers) {
-		console.log('\n');
+		 	//using inquirer to display the question and at the same time take the answer as input
+			inquirer.prompt([
+				{
+					type: 'input',
+					message: clozeQuestions[currentQuestion].partial + '\nAnswer: ',
+					name: 'userAnswer'
+				}
+			]).then(function (answers) {
 
-		// Check if the user answered correctly
-		if (answers.userAnswer.toLowerCase() === clozeQuestions[currentQuestion].cloze.toLowerCase()) {
+							console.log('\n');
 
-			console.log('Your Answer is Correct!');
-			answerRight++;
+							// Check if the user answered correctly
+							if (answers.userAnswer.toLowerCase() === clozeQuestions[currentQuestion].cloze.toLowerCase()) {
+										//adding to our answerRight counter
+										console.log('Your Answer is Correct!');
+										answerRight++;
 
-		} 
-		else {
+							} 
+							else {
+										//adding to our answerWrong counter
+										console.log('Nope, Your Answer is Incorrect!');
+										answerWrong++;
+							}
 
-					console.log('Nope, Your Answer is Incorrect!');
-					answerWrong++;
-		}
-
-				// This will display the correct answer
-				console.log(clozeQuestions[currentQuestion].full);
-				console.log('-------------------------------------\n');
-
-
-		if (currentQuestion < clozeQuestions.length - 1) {
-			currentQuestion++;
-
-					askQuestion();
-
-		} 
-
-		else {
-
-					console.log('Game Over!');
-					console.log('-------------------------------------\n');
-					console.log("Here is your Answers: ")
-					console.log('-------------------------------------\n');
-					console.log('Correct Answers: ' + answerRight);
-					console.log('Incorrect Answers: ' + answerWrong);
-
-					console.log('-------------------------------------\n');
+							// This will display the correct answer
+							console.log(clozeQuestions[currentQuestion].full);
+							console.log('-------------------------------------\n');
 
 
-			//creating a prompt inquirer function that asks the user if they want to play again		
-		inquirer.prompt([
+							if (currentQuestion < clozeQuestions.length - 1) {
+								currentQuestion++;
 
-						{
-							type: 'confirm',
-							message: 'Play Again?',
-							name: 'playAgain'
+										askQuestion();
+
+							} 
+
+							else {
+
+									console.log('Game Over!');
+									console.log('-------------------------------------\n');
+									console.log("Here is your Answers: ")
+									console.log('-------------------------------------\n');
+									console.log('Correct Answers: ' + answerRight);
+									console.log('Incorrect Answers: ' + answerWrong);
+
+									console.log('-------------------------------------\n');
+
+
+									//creating a prompt inquirer function that asks the user if they want to play again		
+									inquirer.prompt([
+
+													{
+														type: 'confirm',
+														message: 'Play Again?',
+														name: 'playAgain'
+													}
+
+									]).then(function (answers) {
+
+															// if user chose play again
+															if (answers.playAgain) {
+																//reset our counters
+																currentQuestion = 0;
+																answerRight = 0;
+																answerWrong = 0;
+
+																//call the function to start the questions
+																askQuestion();	
+
+															} 
+															else {
+																// if user answered no to play again 
+																console.log("See You Next Time pal!");
+
+															}
+									})
+
+									}
+
+
+						})
 						}
 
-		]).then(function (answers) {
-
-				// if user chose play again
-				if (answers.playAgain) {
-					//reset our counters
-					currentQuestion = 0;
-					answerRight = 0;
-					answerWrong = 0;
-
-					//call the function to start the questions
-					askQuestion();	
-
-				} 
-				else {
-					// if user answered no to play again 
-					console.log("See You Next Time pal!");
-
-				}
-		})
-
-		}
-
-
-})
-}
-
-askQuestion();
+		askQuestion();
 
          
 
--- flashCards.js
+++ flashCards.js
@@ -1,29 +1,9 @@
-
-
-
-
-
-//Creating Basic cards constructor
-exports.BasicCard = function(front, back) {
-	
-	this.front = front;
-	this.back = back;
-
-
-}
-
 //Creating Cloze cards constructor
 exports.ClozeCard = function(text, cloze){
 	
 	// this will convert strings to lower case
 	var textToLower = text.toLowerCase();
 	var clozeToLower = cloze.toLowerCase();
-
-	if (!textToLower.includes(clozeToLower)) {
-
-		console.log('ERROR: cloze-deletion does not appear within full text -- <' + cloze + '>');
-		return;
-	}
 
 	this.full = text;
 	this.cloze = cloze;
