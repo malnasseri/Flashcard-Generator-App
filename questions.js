22d756c3c0705043f4d5bf1b9e4fad13799eafc1
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2017-06-21 00:10:41 -0400

    Added questions, comments

---
+++ questions.js
@@ -0,0 +1,74 @@
+exports.questions = [
+	{
+		full:  "In the 2001 comedy film Zoolander Ben Stiller's profession was a Model",
+		cloze: "Model"
+	},
+	{
+		full: "In the 1994 superhero comedy film 'The Mask' the color of the suit that Jim Carey's character wore when he donned the mask was Yellow",
+		cloze: "Yellow"
+	},
+	{
+		full: "50 'First Dates' was the title of an Adam Sandler/Drew Barrymore comedy",
+		cloze: "50"
+	},
+	{
+		full: "The title of the 1987 classic comedy film is 'Good Morning Vietnam'",
+		cloze: "Vietnam"
+	},
+	{
+		full: "Steve Carrell was the lead role of 'Evan Baxter' in the 2007 film 'Evan Almighty'",
+		cloze: "Steve Carrell"
+	},
+	{
+		full: "Need for Speed is a film based on a series of video games released by Electronic Arts 2014",
+		cloze: "Need for Speed"
+	},
+	{
+		full: "The nickname of the character Arnold Schwarzenegger plays in the 1987 film 'Predator' was Dutch",
+		cloze: "Dutch"
+	},
+	{
+		full: "According to the title of the 2011 movie, 'The First Avenger' was Captain America",
+		cloze: "Captain America"
+	},
+	{
+		full: "Tom Cruise starred alongside Cameron Diaz in the 2010 action-comedy movie 'Knight and Day'",
+		cloze: "Tom Cruise"
+	},
+	{
+		full: " Directed by Clint Eastwood, the 2014 action-drama film that was based on the autobiography of 'Chris Kyle' is American Sniper",
+		cloze: "American Sniper"
+	},
+	{
+		full: "Hugh Jackman plays the role of 'Wolverine' in the X Men series of action movies",
+		cloze: "Hugh Jackman"
+	},
+	{
+		full: "John Travolta plays the lead role in the universally panned 2000 film 'Battlefield Earth'?",
+		cloze: "John Travolta"
+	},
+	{
+		full: "The animated sci-fi sitcom 'Futurama' was set in the 31st century",
+		cloze: "31st"
+	},
+	{
+		full: "Cosmo is the name of Jerry's eccentric neighbor from across the hall on the TV sitcom 'Seinfeld'",
+		cloze: "Cosmo"
+	},
+	{
+		full: "Dino is the name of the Flintstone's pet",
+		cloze: "Dino"
+	},
+	{
+		full: "Milhouse is the name of Bart Simpson's best friend",
+		cloze: "Milhouse"
+	},
+	{
+		full: "'Jafar' is the villain in Aladdin  the Disney animated film",
+		cloze: "Jafar"
+	},
+	{
+		full: "In 'Toy Story 2'Woody's Arm was ripped off forcing Andy to leave him behind",
+		cloze: "Arm"
+	}
+]
